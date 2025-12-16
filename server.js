19bf4a21a5983e5797016223454e090e9db24a9a
Truong Minh Thang <truongminhthang10@gmail.com>
2025-12-17 05:57:37 +0700

    sua lại connect

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Kết nối Database Cloud (Sử dụng MongoDB Atlas miễn phí)
-mongoose.connect('mongodb+srv://user:pass@cluster.mongodb.net/test');
+mongoose.connect('mongodb+srv://truongminhthang10_db_user:Password123@cluster0.afyfp3v.mongodb.net/?appName=Cluster0');
 
 app.get('/', (req, res) => {
     res.send('Project 2: Node.js + MongoDB đang chạy!');
